60d834a6df6ca09c449341945c97e80aaabcfefb
Mikolaj Szymczyk <mikolaj.szymczyk.99@gmail.com>
2022-03-12 16:03:15 +0100

    implement FileLoader and some test data

--- lab2/source/ReviewingFilesApp/src/main/java/pl/edu/pwr/FileLoader.java
+++ lab2/source/ReviewingFilesApp/src/main/java/pl/edu/pwr/FileLoader.java
@@ -1,5 +1,6 @@
 package pl.edu.pwr;
 
+import javax.imageio.ImageIO;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
@@ -8,19 +9,29 @@ import java.util.Scanner;
 
 public class FileLoader {
     public static void main(String[] args) {
-
+        FileLoader fileLoader = new FileLoader(Path.of("C:\\Users\\Hyperbook\\Desktop\\JavaZaw\\mszymczyk_248881_java\\lab2\\release\\test\\248881"));
+        try {
+            PersonData personData = fileLoader.readFromRecordFile();
+            System.out.println(personData);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
-    private Path pathToDirectory;
+    public FileLoader(Path pathToDirectory) {
+        this.pathToDirectory = pathToDirectory;
+    }
+
+    private final Path pathToDirectory;
 
     private PersonData readFromRecordFile() throws IOException {
-        File file = new File(pathToDirectory.toFile() + "//" + "record.txt");
+        File file = new File(pathToDirectory.toFile() + "\\record.txt");
         String text;
         Scanner myReader = new Scanner(file);
         //while (myReader.hasNextLine()) {
         String[] data = myReader.nextLine().split(";");
         myReader.close();
-        return new PersonData(data[0], data[1], data[2], data[3], Integer.parseInt(data[4]));
+        return new PersonData(data[0], data[1], data[3], data[4], Integer.parseInt(data[2]), ImageIO.read(new File(pathToDirectory + "\\image.png")));
         //}
 
     }
--- lab2/source/ReviewingFilesApp/src/main/java/pl/edu/pwr/PersonData.java
+++ lab2/source/ReviewingFilesApp/src/main/java/pl/edu/pwr/PersonData.java
@@ -1,14 +1,30 @@
 package pl.edu.pwr;
 
-public class PersonData {
-    String name,surname, course,university;
-    int age;
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
 
-    public PersonData(String name, String surname, String course, String university, int age) {
+public class PersonData {
+    String name, surname, course, university;
+    int age;
+    BufferedImage myPicture;
+
+    public PersonData(String name, String surname, String course, String university, int age, BufferedImage myPicture) {
         this.name = name;
         this.surname = surname;
         this.course = course;
         this.university = university;
         this.age = age;
+        this.myPicture = myPicture;
+    }
+
+    @Override
+    public String toString() {
+        return "PersonData{" +
+                "name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", course='" + course + '\'' +
+                ", university='" + university + '\'' +
+                ", age=" + age +
+                '}';
     }
 }
