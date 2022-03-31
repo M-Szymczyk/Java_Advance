b00791414ed8627e03da082958823e45322f0575
Mikolaj Szymczyk <mikolaj.szymczyk.99@gmail.com>
2022-03-31 12:17:16 +0200

    add listViews listeners, implement them

---
+++ lab4/source/classLoader/src/main/java/pl/edu/pwr/Main.java
@@ -0,0 +1,9 @@
+package pl.edu.pwr;
+
+import pl.edu.pwr.classloader.HelloApplication;
+
+public class Main {
+    public static void main(String[] args) {
+        HelloApplication.main(args);
+    }
+}
--- lab4/source/classLoader/src/main/java/pl/edu/pwr/classloader/HelloController.java
+++ lab4/source/classLoader/src/main/java/pl/edu/pwr/classloader/HelloController.java
@@ -3,19 +3,17 @@ package pl.edu.pwr.classloader;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.DirectoryChooser;
 import pl.edu.pwr.loader.CustomClassLoader;
 
 import java.lang.module.Configuration;
 import java.lang.module.ModuleFinder;
-import java.net.URL;
 import java.nio.file.*;
-import java.util.List;
-import java.util.ResourceBundle;
-import java.util.Set;
+import java.util.*;
 
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
@@ -27,25 +25,51 @@ import java.util.concurrent.Executors;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-public class HelloController implements Initializable {
+/**
+ * Class to store class reference and class loader
+ */
+class LoadedClassPackage {
+    Class<?> classReference;
+    ModuleLayer moduleLayerReference;
+
+    public LoadedClassPackage(Class<?> classRef, ModuleLayer moduleLayer) {
+        this.classReference = classRef;
+        this.moduleLayerReference = moduleLayer;
+    }
+}
+
+public class HelloController{
+    Path chosenPath;
+    String ModuleName = "processors";
+    Set<String> rootModules = Set.of(ModuleName);
+    Map<String, LoadedClassPackage> loadedClass = new HashMap<>();
     @FXML
     public Button chooseFirBtn;
-    Path chosenPath = Paths.get("C:\\Users\\Hyperbook\\Desktop\\JavaZaw\\mszymczyk_248881_java\\lab4\\source\\processors\\out\\production\\processors");
     @FXML
-    ListView<String> PossibleToLoadList;
+    ListView<String> PossibleToLoadList, LoadClassList;
 
+    /**
+     * Method calls for directory chooser
+     */
     @FXML
     protected void chooseDir() {
         chosenPath = new DirectoryChooser().showDialog(null).toPath();
-        load();
+        loadFiles();
     }
 
+    /**
+     * Method add list of files to ListView
+     */
     private void loadFiles() {
         ObservableList<String> items = FXCollections.observableArrayList(
                 listFilesUsingJavaIO());
         PossibleToLoadList.setItems(items);
     }
 
+    /**
+     * Method searches for class in directory
+     * @return list of files
+     */
     public List<String> listFilesUsingJavaIO() {
 
         try (Stream<Path> stream = Files.walk(chosenPath)) {
@@ -58,48 +82,32 @@ public class HelloController implements Initializable {
         } catch (IOException e) {
             e.printStackTrace();
         }
-
         return null;
     }
 
-
-    private void load() {
+    /**
+     * Method invoke method submit task, with loaded Status listener, when processing fishing display result
+     * @param loadedClassPackage reference to class and, module layer
+     */
+    private void invokeSubmittal(LoadedClassPackage loadedClassPackage) {
         try {
-            // Define the set of root modules to be resolved in the custom layer
-            String ModuleName = "processors";
-            Set<String> rootModules = Set.of(ModuleName);
-            // Create a custom layer
-            ModuleLayer customLayer = createLayer(chosenPath.toString(), rootModules);
-
-
-            Class<?> loadedToUpperClass = customLayer.findLoader(ModuleName).loadClass("pl.edu.pwr.lib.ToUpperCase"),
-                    loadedStatusListener = customLayer.findLoader(ModuleName).loadClass("pl.edu.pwr.lib.SomeStatusListener"),
-                    loadedStatusListenerInterface = customLayer.findLoader(ModuleName).loadClass("pl.edu.pwr.processing.StatusListener");
-
-            Object classInstance = loadedToUpperClass.getConstructor().newInstance();
-
-
-            Method getInfoMethod = loadedToUpperClass.getDeclaredMethod("getInfo");
-            System.out.println((String) getInfoMethod.invoke(classInstance));
-            Method submitTaskMethod = loadedToUpperClass.getDeclaredMethod("submitTask", String.class, loadedStatusListenerInterface);
+            Class<?> loadedStatusListener = loadedClassPackage.moduleLayerReference.findLoader(ModuleName).loadClass("pl.edu.pwr.processing.SomeStatusListener"),
+                    loadedStatusListenerInterface = loadedClassPackage.moduleLayerReference.findLoader(ModuleName).loadClass("pl.edu.pwr.processing.StatusListener");
+            Method submitTaskMethod = loadedClassPackage.classReference.getMethod("submitTask", String.class, loadedStatusListenerInterface);
+            Object classInstance = loadedClassPackage.classReference.getConstructor().newInstance();
             boolean b = (boolean) submitTaskMethod.invoke(classInstance, "Tekst na wejście", loadedStatusListener.getConstructor().newInstance());
             if (b)
                 System.out.println("Processing started correctly");
             else
                 System.out.println("Processing ended with an error");
-            Method getResultMethod = loadedToUpperClass.getDeclaredMethod("getResult");
+            Method getResultMethod = loadedClassPackage.classReference.getDeclaredMethod("getResult");
             ExecutorService executor = Executors.newSingleThreadExecutor();
 
-            // uruchom zadanie, które skończy się, gdy result!=null
             executor.submit(() -> {
                 String result = null;
                 while (true) {
-                    // System.out.println(scheduleFuture.isDone());
-
                     try {
                         Thread.sleep(800);
-
-                        // String result = (String) getResultMethod.invoke(o,new Object[] {});
                         result = (String) getResultMethod.invoke(classInstance);
                     } catch (InterruptedException | IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
                         e.printStackTrace();
@@ -120,28 +128,27 @@ public class HelloController implements Initializable {
 
     }
 
+    /**
+     * Method create Module layer
+     * @param modulePath path to module
+     * @param rootModules set of modules to load
+     * @return ModuleLayer for given class
+     * @throws IllegalAccessException
+     */
     public static ModuleLayer createLayer(String modulePath, Set<String> rootModules) throws IllegalAccessException {
         Path path = Paths.get(modulePath);
 
-        // Define the module finders to be used in creating a
-        // configuration for the custom layer
         ModuleFinder beforeFinder = ModuleFinder.of(path);
         ModuleFinder afterFinder = ModuleFinder.of();
 
-        // Create a configuration for the custom layer
         Configuration parentConfig = ModuleLayer.boot().configuration();
         Configuration config =
                 parentConfig.resolve(beforeFinder, afterFinder, rootModules);
 
-        /* Create a custom layer with one class loader. The parent for
-           the class loader is the system class loader. The boot layer is
-           the parent layer of this custom layer.
-         */
         ClassLoader sysClassLoader = new CustomClassLoader(path);
         ModuleLayer parentLayer = ModuleLayer.boot();
         ModuleLayer layer = parentLayer.defineModulesWithOneLoader(config, sysClassLoader);
 
-        // Check if we loaded the module in this layer
         if (layer.modules().isEmpty()) {
             System.out.println("\nCould not find the module " + rootModules
                     + " at " + modulePath + ". "
@@ -150,8 +157,52 @@ public class HelloController implements Initializable {
         return layer;
     }
 
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
-        loadFiles();
+    /**
+     * Method load class and move item from list PossibleToLoad to loadClassList
+     */
+    public void loadClass() {
+        String className = PossibleToLoadList.getSelectionModel().getSelectedItems().get(0);
+        ObservableList<String> list = PossibleToLoadList.getItems();
+        list.remove(className);
+        PossibleToLoadList.setItems(list);
+        try {
+
+            ModuleLayer customLayer = createLayer(chosenPath.toString(), rootModules);
+
+            Class<?> loadedToUpperClass = customLayer.findLoader(ModuleName).loadClass("pl.edu.pwr.lib." + className.replace(".class", ""));
+            Object classInstance = loadedToUpperClass.getConstructor().newInstance();
+            loadedClass.put(className,new LoadedClassPackage(loadedToUpperClass,customLayer) );
+            Method getInfoMethod = loadedToUpperClass.getDeclaredMethod("getInfo");
+            className += " | " + getInfoMethod.invoke(classInstance);
+            ObservableList<String> list1 = LoadClassList.getItems();
+            list1.add(className);
+            LoadClassList.setItems(list1);
+        } catch (IllegalAccessException | ClassNotFoundException | InvocationTargetException | InstantiationException | NoSuchMethodException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Method is listener for LoadClassList
+     * if item is clicked with right mouse button, then selected class is unloaded and move to PossibleToLoadList
+     * if item is clicked with left mouse button,then on selected class method invokeSubmittal is called
+     *
+     * @param mouseEvent require to check button
+     */
+    public void runClass(MouseEvent mouseEvent) {
+        if (mouseEvent.getButton().equals(MouseButton.PRIMARY)) {
+            String chosen = LoadClassList.getSelectionModel().getSelectedItems().get(0);
+            invokeSubmittal(loadedClass.get(chosen.split(" | ")[0]));
+
+        } else if (mouseEvent.getButton().equals(MouseButton.SECONDARY)) {
+            String chosen = LoadClassList.getSelectionModel().getSelectedItems().get(0);
+            ObservableList<String> list = LoadClassList.getItems();
+            list.remove(chosen);
+            LoadClassList.setItems(list);
+            loadedClass.remove(chosen.split(" | ")[0]);
+            ObservableList<String> list1 = PossibleToLoadList.getItems();
+            list1.add(chosen.split(" | ")[0]);
+            PossibleToLoadList.setItems(list1);
+        }
     }
 }
